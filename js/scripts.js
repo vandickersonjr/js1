74de59b7be58db6af7da8b8fe85b2f85c5e8c213
unknown <vandickersonjr@gmail.com>
2018-05-22 20:12:55 -0700

    completed take home

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-var myName = "Van";
+var myName = "My goals for this class are to get a basic understanding of JavaScript and connect with other developers. Coding is my hobby.";
 
 console.log(myName);
