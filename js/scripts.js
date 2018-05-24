bdee6e226c73820b87846d6b229e0171964b63b7
unknown <vandickersonjr@gmail.com>
2018-05-24 08:07:50 -0700

    day2 practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,40 @@
+/*
 var myName = "Van Dickerson";
 
-console.log(myName);
+let myBirthdate = new Date('11.17.85');
 
-var myGoal = "My goals for this class are to get a basic understanding of JavaScript and connect with other developers.";
+const myGoal = "My goals for this class are to get a basic understanding of JavaScript and connect with other developers.";
 
-console.log(myGoal);
+console.log(myName + myBirthdate + myGoal);
 
-var myHobby = "Coding is my hobby.";
+console.log(myName, myBirthdate, myGoal);
 
-console.log(myHobby);
+alert(myGoal);
+
+let userAnswer = prompt("Did you finish your homework?", "Yes");
+
+console.log(userAnswer);
+*/
+
+/*
+1. Declare 2 let variables:
+  a. Favorite movie
+  b. Favorite food
+2. Declare 2 const variables:
+  a. Year the movie came out
+  b. Ingredients of your favorite foods as an array
+3. Alert your favorite food with its Ingredients
+4. Confirm the movie and year
+*/
+
+var favMovie = "Black Panther";
+
+var favFood = "Sandwiches";
+
+const year = new Date('2/16/2018');
+
+const ingredients = ['sourdough', 'bread', 'meat', 'chesse', 'spinach', 'avacado'];
+
+alert(favFood + " " + ingredients);
+
+confirm(favMovie + " " + year);
