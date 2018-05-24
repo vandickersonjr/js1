37af745a2f484bb7baa3969cdde5e417d90ec034
unknown <vandickersonjr@gmail.com>
2018-05-23 19:10:10 -0700

    completed takehome

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-var myName = "My goals for this class are to get a basic understanding of JavaScript and connect with other developers. Coding is my hobby.";
+var myName = "Van Dickerson";
 
 console.log(myName);
+
+var myGoal = "My goals for this class are to get a basic understanding of JavaScript and connect with other developers.";
+
+console.log(myGoal);
+
+var myHobby = "Coding is my hobby.";
+
+console.log(myHobby);
