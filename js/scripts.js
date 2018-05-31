b3a74c0bae4eccc31e0bd6c85ba713eb19362430
unknown <vandickersonjr@gmail.com>
2018-05-30 19:22:37 -0700

    day 3 branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-var myName = "Van";
+prompt('What is your favorite color?')
 
-console.log(myName);
+let favColor = prompt('give your fav', '...')
+
+switch (favColor.upperLowerCase()) //{normalize the data
+  case 'blue':
+      alert('${favColor} is my favorite tool!');
+    break;
+  default:
+      alert('${myName} not a valid options!')
+
+}
