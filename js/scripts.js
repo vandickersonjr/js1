ee79dd8fb97e9ba1f6917bec516b9fe0e2be0fed
vandickersonjr <36873559+vandickersonjr@users.noreply.github.com>
2018-06-02 10:24:56 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,2 @@
 
-let firstName = prompt("What is your first name?");
 
-console.log(firstName)
-
-let lastName = prompt("What is your last name?");
-
-alert(`Hello, ${lastName}!`);
-
-let birthdate = prompt("What is your birthdate?");
-
-confirm(birthdate);
-
-alert(birthdate);
