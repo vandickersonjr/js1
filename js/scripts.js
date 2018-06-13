cbc98abb8354e92bac52088b2b9f95f3a730b75e
unknown <vandickersonjr@gmail.com>
2018-06-13 08:40:13 -0700

    new day6 branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,44 @@
-var myName = "Van";
+let body = document.getElementsByTagName('body');
 
-console.log(myName);
+console.log(body[0]);
+
+let classes = document.getElementsByClassName("my-header");
+
+console.log(classes[0]);
+
+let id = document.getElementById("my-paragraph");
+
+console.log(id);
+
+// Newer Methods:
+let tag = document.querySelector('body');
+
+console.log(tag);
+
+let myClass = document.querySelector('.my-header');
+
+console.log(myClass);
+
+let myId = document.querySelector('#my-paragraph');
+
+console.log(myId);
+
+let tags = document.querySelectorAll('body');
+
+console.log(tags);
+
+// Try it out
+function queryDom(methodName, element) {
+  switch(methodNamecase.toLowerCase()) {
+    case 'getelementsbytagname':
+    document.getElementsByTagName(element);
+    break;
+    case 'queryselector': document.querySelector(`.${element}`);
+    break;
+    default:
+    break;
+  }
+};
+
+myClass.addEventListener('click', e => { // first argument is a valid string
+})
